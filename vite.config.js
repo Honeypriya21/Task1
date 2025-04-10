9f53437b3374a0ddbb35532f9e1297f4bad7eb7b
HoneyPriya21 <bathulahoneypriyadarshini@gmail.com>
2025-04-10 11:13:12 +0530

    upp

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hello-world-vite/'  // Replace with your repo name
+  base: 'Task1'  // Replace with your repo name
 })
